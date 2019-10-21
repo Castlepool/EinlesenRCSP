b6257736fcfa80e669e375d388e8870d2fc78aed
Castlepool <43089783+Castlepool@users.noreply.github.com>
2019-10-21 12:48:22 +0200

    first commit

---
+++ src/Test.java
@@ -0,0 +1,60 @@
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Test {
+	
+	
+	
+	
+	public static void main (String[] args) throws FileNotFoundException{
+//		Job[] jobs     = Job.read(new File("j1201_5.sm"));//best makespan=112
+//		Resource[] res = Resource.read(new File("j1201_5.sm"));
+		Job[] jobs     = Job.read(new File("j12046_8.sm"));
+		Resource[] res = Resource.read(new File("j12046_8.sm"));
+
+		
+		
+		for(int i = 0; i < jobs.length; i++){
+			jobs[i].calculatePredecessors(jobs);
+		}
+		auslesen(jobs);
+		auslesen(res);
+	}
+	
+	
+	
+	
+	
+	
+	
+	private static void auslesen(Job[] jobs) {
+		int gesamtDauer = 0;
+		for (int i = 0; i < jobs.length; i++){
+			gesamtDauer += jobs[i].dauer();
+			
+			System.out.printf("Nummer: %3d | Nachfolger: %-15s | Vorgaenger: %-15s | Dauer: %2d | R1: %2d R2: %2d R3: %2d R4: %2d | %n",
+					jobs[i].nummer(),
+					jobs[i].nachfolger().toString(),
+					jobs[i].vorgaenger().toString(),
+					jobs[i].dauer(),
+					jobs[i].verwendeteResource(0),
+					jobs[i].verwendeteResource(1),
+					jobs[i].verwendeteResource(2),
+					jobs[i].verwendeteResource(3)
+				);
+		}
+		System.out.println("T = " + gesamtDauer);
+	}
+	
+	private static void auslesen(Resource[] resource) {
+		for (int i = 0; i < resource.length; i++){
+			System.out.print("Resource: " + resource[i].nummer()+"     |    ");
+			System.out.println("Verf�gbarkeit: " + resource[i].maxVerfuegbarkeit());
+		}
+	}
+	
+	
+}
